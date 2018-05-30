d7cfb8ebf78fadf121fc18e7d294cb8ef08154fe
ashrafmisran <ashrafmisran@gmail.com>
2018-05-30 08:04:13 +0800

    Navbar > + > Student,enroll,tutor,class

--- navbar.php
+++ navbar.php
@@ -10,6 +10,16 @@
         <a class="nav-link" href="?m=">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-plus"></i></a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="?m=add-student">New student</a>
+          <a class="dropdown-item" href="?m=enroll-student">New enrollment</a>
+          <div role="separator" class="dropdown-divider"></div>
+          <a class="dropdown-item" href="?m=add-tutor">New tutor</a>
+          <a class="dropdown-item" href="?m=add-class">New class</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Students Management</a>
         <div class="dropdown-menu">
           <a class="dropdown-item" href="?m=student-registration">Registration</a>
